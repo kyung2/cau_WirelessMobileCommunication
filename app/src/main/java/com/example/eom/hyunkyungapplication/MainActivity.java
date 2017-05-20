7b3726be449767e4594999a0d8035d718d345783
kyung2 <lovelyhyunkyung@gmail.com>
2017-05-20 22:18:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/eom/hyunkyungapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.eom.hyunkyungapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekbar;
+    TextView soju;
+    TextView beer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        soju = (TextView)findViewById(R.id.textView4);
+        beer = (TextView)findViewById(R.id.textView6);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser) {
+                int tmp = 10-progress;
+                soju.setText(progress + "");
+                beer.setText(tmp + "");
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+    }
+}
+
