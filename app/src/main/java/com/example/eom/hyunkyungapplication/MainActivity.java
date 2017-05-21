2aaf88b40768a381a4c77e559749c08b135ba5d6
kyung2 <lovelyhyunkyung@gmail.com>
2017-05-21 14:56:30 +0900

    add event

--- app/src/main/java/com/example/eom/hyunkyungapplication/MainActivity.java
+++ app/src/main/java/com/example/eom/hyunkyungapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekbar;
     TextView soju;
     TextView beer;
+    int cup_size = 0;
+    int soju_time = 0;
+    int beer_time = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
         soju = (TextView)findViewById(R.id.textView4);
         beer = (TextView)findViewById(R.id.textView6);
 
+        ImageButton cup_samll = (ImageButton)findViewById(R.id.imageButton1) ;
+        ImageButton cup_medium = (ImageButton)findViewById(R.id.imageButton2) ;
+        ImageButton cup_large = (ImageButton)findViewById(R.id.imageButton3) ;
+
+
+
+        Button payment = (Button)findViewById(R.id.button) ;
+
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser) {
                 int tmp = 10-progress;
@@ -34,6 +46,38 @@ public class MainActivity extends AppCompatActivity {
             public void onStartTrackingTouch(SeekBar seekBar) {}
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
+
+        cup_samll.setOnClickListener(new Button.OnClickListener() {
+            @Override public void onClick(View view) {
+                cup_size = 50;
+                soju_time = 50;
+                beer_time = 50;
+            }
+        }) ;
+
+        cup_medium.setOnClickListener(new Button.OnClickListener() {
+            @Override public void onClick(View view) {
+                cup_size = 180;
+                soju_time = 50;
+                beer_time = 50;
+            }
+        }) ;
+
+
+        cup_large.setOnClickListener(new Button.OnClickListener() {
+            @Override public void onClick(View view) {
+                cup_size = 300;
+                soju_time = 50;
+                beer_time = 50;
+            }
+        }) ;
+
+        payment.setOnClickListener(new Button.OnClickListener() {
+            @Override public void onClick(View view) {
+                // nfc 페이지
+            }
+        }) ;
+
 
     }
 }
